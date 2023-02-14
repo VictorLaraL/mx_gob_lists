437a79c2fe90c49206647a4f58261ac777e1bfb6
Victor Lara <victorlara@logicsystems.com.mx>
2023-02-14 16:13:07 -0600

    feat

--- app/SAT69B/process_data.py
+++ app/SAT69B/process_data.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import schedule
+import time
 
 from .webscraping import webscrapping_69b
 
 def formating_data():
-    pass
+    df = webscrapping_69b()
 
 def migrate_data():
-    df = webscrapping_69b()
+    pass
+
+# Program when need to get the list.
+schedule.every().day.at('06:00').do(formating_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
